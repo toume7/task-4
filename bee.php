41fcbe55cb69025c07910e7bed2de470b8890b05
toume7 <91023807+toume7@users.noreply.github.com>
2021-10-07 21:45:41 +0400

    Update bee.php

--- bee.php
+++ bee.php
@@ -12,68 +12,8 @@ class bee implements gibel
     public $hierarchy;
     public $varieties;
 
-    /**
-     * bee constructor.
-     * @param $hierarchy
-     * @param $varieties
-     */
-    public function __construct($hierarchy, $varieties)
-    {
-        $this->hierarchy = $hierarchy;
-        $this->varieties = $varieties;
-    }
-
-    public function arrive()
-    {
-        /**
-         * код рождения
-         */
-    }
-
-    public function grow_up()
-    {
-        /**
-         * рост
-         */
-    }
-
-    public function collection()
-    {
-        /**
-         * код  сборы пыльцы
-         */
-    }
-
-    public function care()
-    {
-        /**
-         * код заботы о потомстве
-         */
-    }
 
     public function doome(){
         echo 'гибель';
     }
 
-    public function Display()
-    {
-        echo('Иерархия- ' . $this->hierarchy . '<br>' . 'разновидность- ' . $this->varieties . '<br>');
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getHierarchy()
-    {
-        return $this->hierarchy;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getVarieties()
-    {
-        return $this->varieties;
-    }
-
-}
